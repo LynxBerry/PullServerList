50f99788f1ffe8efed55c4b0077dc10ab784c759
Steven Shao <stevensh@microsoft.com>
2016-06-30 11:33:05 +0800

    updated for changeset.ps1

--- ChangeSet.ps1
+++ ChangeSet.ps1
@@ -1,1 +1,21 @@
 # Change Set concept
+
+$sampleChangeSet = @{
+    "SequenceName" = "BJB_Patch_By_Scripts";
+    "BatchName" = "cp-host-1";
+    "Add" = @("server1", "server2");
+    "Remove" = @("server3", "server4");
+    "SnapshotTag" = "Snapshot06202016_11_33_51";
+}
+
+
+# Generate Change Set.
+# Can use format of Json.
+# Generation happens each time the script scans the difference.
+
+#--- Use a seperate command to commit the change ---#
+# Backup the target server list file.
+
+# Write log about the change.
+
+# Commit the change set to the target server list file.
