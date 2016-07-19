d74e6757846be3b776f3421b2bf82251765654bd
Steven Shao <stevensh@microsoft.com>
2016-07-19 13:36:33 +0800

    ChangeSet.ps1 updated

--- ChangeSet.ps1
+++ ChangeSet.ps1
@@ -12,6 +12,65 @@ $sampleChangeSet = @{
 # Generate Change Set.
 # Can use format of Json.
 # Generation happens each time the script scans the difference.
+# Generate from where: diff result.
+# Change Sets ==> Change Set
+function genChangeSets {
+    param(
+        $diffResults
+    )
+    $changeSets = @{}
+    foreach ($diffResult in $diffResults) {
+        $items = $diffResult.InputObject.split(":")
+        if ($changeSets.keys -notcontains $items[0]) {
+            $changeSets[$items[0]] = @{}
+        }
+
+        $action = ""
+        if ($diffResult.SideIndicator -eq "=>") {
+            $action = "add"
+        } else {
+            $action = "remove"
+        }
+
+        $($changeSets[$items[0]])[$action] += @($items[1])
+     }
+
+     $changeSets
+
+}
+
+function genChangeSet {
+    param(
+        [hash] $changeSets
+    )
+
+    foreach ($changeSet in $changeSets.GetEnumerator()) {
+        $keyname = $changeSet.key
+        # unfinished
+    }
+}
+
+# Test
+$left = @(
+    '\APClientPROD.xml\apclient-1st.txt:DM2SP1PINBHCA01',     
+    '\APClientPROD.xml\apclient-4th.txt:CO1SOLBHCBN05'    
+)
+
+$right = @(
+    '\BJB_Patch_By_Scripts\cp-1.txt:BJBCCOPSFTP01',  
+    '\BJB_Patch_By_Scripts\cp-1.txt:BJBCCSQLBAKA03', 
+    '\BJB_Patch_By_Scripts\cp-1.txt:BJBCSMEAD02',    
+    '\BJB_Patch_By_Scripts\cp-1.txt:SHACSMEADMUTL02',
+    '\BJB_Patch_By_Scripts\cp-2.txt:BJBCSMEAD01',    
+    '\BJB_Patch_By_Scripts\cp-2.txt:SHACSMEADMDEP02',
+    '\BJB_Patch_By_Scripts\cp-2.txt:SHACSMEADMUTL01'
+
+)
+
+$diffResults = diff $left $right
+#echo $diffResults
+#echo "==="
+genChangeSets -diffResults $diffResults | convertTo-Json
 
 #--- Use a seperate command to commit the change ---#
 # Backup the target server list file.
